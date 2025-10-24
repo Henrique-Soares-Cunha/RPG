0c0fc3910c6421f83d31db66b34458670adf9b49
Henrique <henrique.sc2@puccampinas.edu.br>
2025-10-24 19:16:25 -0300

    leitor de arquivos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        for (int i = 1 ; i < 10 ; i++){
-            System.out.println(Dados.D20());
+        leitorDeArquivos historia = new leitorDeArquivos();
+
+        try {
+            historia.lerArquivo();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
     }
